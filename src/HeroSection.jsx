8968c4b3b9201a080b46bba4ba2bedff222543f5
Remy <shingiroremy303@gmail.com>
2025-11-21 19:46:23 +0200

    Added the middle center

--- src/HeroSection.jsx
+++ src/HeroSection.jsx
@@ -27,15 +27,45 @@ function Hero(){
             </div>
         </div>
 
-        <div class="w-289 h-80 bg-gray-40">
+        <div class="w-289 h-80 bg-gray-200">
             <div class="text-4xl font-bold absolute left-50 mt-20">
                 <p class="text-gray-500">Optimistic and forward-looking</p>
                 <p class="text-gray-500">agent of central government, working to achieve</p>
                 <p class="text-blue-800">the financial security and health of</p>
                 <p class="text-blue-800">all Rwandans.</p>
             </div>
+        </div>
 
+        <div class="flex space-x-4 bg-white w-200 h-36 absolute top-164 left-45">
+            <div>
+                <img class="w-12" src="src/assets/public (2).svg"/>
+                <p class="font-bold text-blue-950">Member First</p>
+                <p><span class="text-blue-900 font-bold">13 Million</span> of Rwandans</p>
+                <p> are covered by RSSB services</p>
+            </div>
 
+            <div>
+                <img class="w-12" src="src/assets/public (2).svg"/>
+                <p class="font-bold text-blue-950">Experience</p>
+                <p><span class="text-blue-900 font-bold">60 years</span>of service for your</p>
+                <p> safety and well-being.</p>
+            </div>
+
+            <div>
+                <img class="w-12" src="src/assets/public (1).svg"/>
+                <p class="font-bold text-blue-950">Branches</p>
+                <p>Our services decentralized</p>
+                <p> in<span class="text-blue-900 font-bold">30 Branches</span>for your</p>
+                <p> satisfaction</p>
+            </div>
+
+            <div>
+                <img class="w-12" src="src/assets/public.svg"/>
+                <p class="font-bold text-blue-950">Data Driven</p>
+                <p>Learning from our data to</p>
+                <p>improve the services offered</p>
+                <p>to our members</p>
+            </div>
         </div>
         </>
     )
